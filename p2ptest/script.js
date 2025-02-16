03876ec2791dc1c73139aae7702bbde0c0a065ab
taka2360 <132958770+taka2360@users.noreply.github.com>
2025-02-16 12:46:13 +0900

    update p2ptest

--- p2ptest/script.js
+++ p2ptest/script.js
@@ -1,6 +1,6 @@
 
 // ICE server URLs
-let peerConnectionConfig = {'iceServers': [{"urls": "stun:stun.l.google.com:19302"}]};
+let peerConnectionConfig = { 'iceServers': [{ "urls": "stun:stun.l.google.com:19302" }] };
 
 // Data channel オプション
 let dataChannelOptions = {
@@ -14,17 +14,22 @@ let peerConnection;
 let dataChannel;
 
 // ページ読み込み時に呼び出す関数
-window.onload = function() {
+window.onload = function () {
     document.getElementById('status').value = 'closed';
+    // paramがあったときは自動でセット
+    const params = new URLSearchParams(location.href.search);
+    if (params.size !== 0) {
+        setRemoteSdp(params.get("sdp"))
+    }
 }
 
 // 新しい RTCPeerConnection を作成する
-function createPeerConnection() {
+function createPeerConnection(isfirst) {
     let pc = new RTCPeerConnection(peerConnectionConfig);
 
     // ICE candidate 取得時のイベントハンドラを登録
-    pc.onicecandidate = function(evt) {
-        if ( evt.candidate ) {
+    pc.onicecandidate = function (evt) {
+        if (evt.candidate) {
             // 一部の ICE candidate を取得
             // Trickle ICE では ICE candidate を相手に通知する
             console.log(evt.candidate);
@@ -36,25 +41,44 @@ function createPeerConnection() {
             // 今回は手動でシグナリングするため textarea に SDP を表示する
             document.getElementById('localSDP').value = pc.localDescription.sdp;
             document.getElementById('status').value = 'Vanilla ICE ready';
+            document.getElementById('QR').textContent = '';
+            if (isfirst) {
+                var qrcode = new QRCode('QR', {
+                    text: location.href + "\\?sdp=" + pc.localDescription.sdp,
+                    width: 512,
+                    height: 512,
+                    correctLevel: QRCode.CorrectLevel.H
+                });
+            }else{
+                var qrcode = new QRCode('QR', {
+                    text: "javascript:setRemoteSdp(sdp=\"" + pc.localDescription.sdp + "\")",
+                    width: 512,
+                    height: 512,
+                    correctLevel: QRCode.CorrectLevel.H
+                });
+            }
+
         }
     };
 
-    pc.onconnectionstatechange = function(evt) {
-        switch(pc.connectionState) {
-        case "connected":
-            document.getElementById('status').value = 'connected';
-            break;
-        case "disconnected":
-        case "failed":
-            document.getElementById('status').value = 'disconnected';
-            break;
-        case "closed":
-            document.getElementById('status').value = 'closed';
-            break;
+    pc.onconnectionstatechange = function (evt) {
+        switch (pc.connectionState) {
+            case "connected":
+                document.getElementById('status').value = 'connected';
+                window.alert("接続しました")
+                break;
+            case "disconnected":
+            case "failed":
+                document.getElementById('status').value = 'disconnected';
+                window.alert("disconnectされました")
+                break;
+            case "closed":
+                document.getElementById('status').value = 'closed';
+                break;
         }
     };
 
-    pc.ondatachannel = function(evt) {
+    pc.ondatachannel = function (evt) {
         console.log('Data channel created:', evt);
         setupDataChannel(evt.channel);
         dataChannel = evt.channel;
@@ -66,22 +90,22 @@ function createPeerConnection() {
 // ピアの接続を開始する
 function startPeerConnection() {
     // 新しい RTCPeerConnection を作成する
-    peerConnection = createPeerConnection();
+    peerConnection = createPeerConnection(true);
 
     // Data channel を生成
     dataChannel = peerConnection.createDataChannel('test-data-channel', dataChannelOptions);
     setupDataChannel(dataChannel);
 
     // Offer を生成する
-    peerConnection.createOffer().then(function(sessionDescription) {
+    peerConnection.createOffer().then(function (sessionDescription) {
         console.log('createOffer() succeeded.');
         return peerConnection.setLocalDescription(sessionDescription);
-    }).then(function() {
+    }).then(function () {
         // setLocalDescription() が成功した場合
         // Trickle ICE ではここで SDP を相手に通知する
         // Vanilla ICE では ICE candidate が揃うのを待つ
         console.log('setLocalDescription() succeeded.');
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.error('setLocalDescription() failed.', err);
     });
 
@@ -90,37 +114,53 @@ function startPeerConnection() {
 
 // Data channel のイベントハンドラを定義する
 function setupDataChannel(dc) {
-    dc.onerror = function(error) {
+    dc.onerror = function (error) {
         console.log('Data channel error:', error);
     };
-    dc.onmessage = function(evt) {
+    dc.onmessage = function (evt) {
         console.log('Data channel message:', evt.data);
         let msg = evt.data;
         document.getElementById('history').value = 'other> ' + msg + '\n' + document.getElementById('history').value;
     };
-    dc.onopen = function(evt) {
+    dc.onopen = function (evt) {
         console.log('Data channel opened:', evt);
     };
-    dc.onclose = function() {
+    dc.onclose = function () {
         console.log('Data channel closed.');
     };
 }
 
 // 相手の SDP 通知を受ける
-function setRemoteSdp() {
-    let sdptext = document.getElementById('remoteSDP').value;
+function setRemoteSdp(sdp = "") {
+    let sdptext = null;
+    if (sdp === "") {
+        sdptext = document.getElementById('remoteSDP').value;
+        console.log("手動");
+    } else {
+        sdptext = sdp;
+        console.log("qr");
+    }
 
-    if ( peerConnection ) {
+
+    if (peerConnection) {
         sdptext += "\n";
         // Peer Connection が生成済みの場合，SDP を Answer と見なす
         let answer = new RTCSessionDescription({
             type: 'answer',
             sdp: sdptext,
         });
-        peerConnection.setRemoteDescription(answer).then(function() {
+        peerConnection.setRemoteDescription(answer).then(function () {
             console.log('setRemoteDescription() succeeded.');
-        }).catch(function(err) {
-            console.error('setRemoteDescription() failed.', err);
+        }).catch(function () {
+            answer = new RTCSessionDescription({
+                type: 'answer',
+                sdp: sdptext.slice(0, -1),
+            });
+            peerConnection.setRemoteDescription(answer).then(function () {
+                console.log('setRemoteDescription() succeeded.\n \\n was sliced.');
+            }).catch(function (err) {
+                console.error('setRemoteDescription() failed. check SDP.', err);
+            });
         });
     } else {
         // Peer Connection が未生成の場合，SDP を Offer と見なす
@@ -129,22 +169,22 @@ function setRemoteSdp() {
             sdp: sdptext,
         });
         // Peer Connection を生成
-        peerConnection = createPeerConnection();
-        peerConnection.setRemoteDescription(offer).then(function() {
+        peerConnection = createPeerConnection(false);
+        peerConnection.setRemoteDescription(offer).then(function () {
             console.log('setRemoteDescription() succeeded.');
-        }).catch(function(err) {
+        }).catch(function (err) {
             console.error('setRemoteDescription() failed.', err);
         });
         // Answer を生成
-        peerConnection.createAnswer().then(function(sessionDescription) {
+        peerConnection.createAnswer().then(function (sessionDescription) {
             console.log('createAnswer() succeeded.');
             return peerConnection.setLocalDescription(sessionDescription);
-        }).then(function() {
+        }).then(function () {
             // setLocalDescription() が成功した場合
             // Trickle ICE ではここで SDP を相手に通知する
             // Vanilla ICE では ICE candidate が揃うのを待つ
             console.log('setLocalDescription() succeeded.');
-        }).catch(function(err) {
+        }).catch(function (err) {
             console.error('setLocalDescription() failed.', err);
         });
         document.getElementById('status').value = 'answer created';
@@ -153,7 +193,7 @@ function setRemoteSdp() {
 
 // チャットメッセージの送信
 function sendMessage() {
-    if ( !peerConnection || peerConnection.connectionState != 'connected' ) {
+    if (!peerConnection || peerConnection.connectionState != 'connected') {
         alert('PeerConnection is not established.');
         return false;
     }
